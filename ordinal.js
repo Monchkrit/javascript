29b6b9c5077ecf34a1971058c63417647040e674
Monchkrit <blitzkritter@gmail.com>
2019-04-19 14:04:55 +0000

    New day commit

--- ordinal.js
+++ ordinal.js
@@ -1,17 +1,24 @@
 let readlineSync = require('readline-sync');
 
 let ordinal = function(digit) {
-  digitNum = Number(digit);
+  let digitNum = Number(digit);
   let lastDigit = digitNum % 10;
-  if (lastDigit === 1) {
-
-  return digit + "st";
+  let lastTwoDigits = digitNum % 100;
+  
+  if (lastTwoDigits > 10 && lastTwoDigits < 20) {
+    return digit + "th";
+  }
+  else if (lastDigit === 1) {
+    return digit + "st";
   }
   else if (lastDigit === 1) {
     return digit + "nd";
   }
   else if (lastDigit === 3) {
     return digit + "rd";
+  }
+  else {
+    return digit + "th";
   }
 
 return lastDigit;
